c51e5ef2593fb6f932bdda67eb4f55d66b0b5d04
Pablo <pablo.saenz.b@gmail.com>
2022-05-25 14:07:55 +0200

    Changed ports that caused Angular to break

--- Angular11/frontend/src/app/services/tutorial.service.ts
+++ Angular11/frontend/src/app/services/tutorial.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
-const baseUrl = 'http://localhost:8080/api/tutorials';
+const baseUrl = 'http://localhost:3000/api/tutorials';
 @Injectable({
   providedIn: 'root'
 })
